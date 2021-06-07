a5ae3d04ff5a883176de9c654b03e07b9dd90f3f
Clark Eagling - ee33 <37630289+ClarkEagling@users.noreply.github.com>
2021-06-07 10:20:55 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,33 @@ namespace SpriteKind {
     export const Invincible = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile4`, function (sprite, location) {
-    game.over(true, effects.blizzard)
+    game.over(false, effects.blizzard)
 })
+function CreatePlayer () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
+        . . . 8 8 8 8 8 8 8 8 8 8 8 . . 
+        8 8 8 8 8 8 8 c 8 8 8 c 8 8 . . 
+        . 8 8 8 8 8 1 1 c 8 c 1 1 1 8 . 
+        . 8 8 8 8 1 1 f 1 c 8 1 f 1 8 . 
+        8 8 8 8 8 8 1 1 1 8 8 1 1 1 8 . 
+        . 8 8 8 8 8 9 8 8 8 8 8 9 8 8 . 
+        8 8 8 8 8 8 9 8 8 8 9 9 9 8 f f 
+        . 8 8 8 4 8 8 4 4 4 9 4 4 f f f 
+        . 1 1 8 4 8 8 4 4 4 9 4 4 4 8 . 
+        . 1 1 4 4 8 8 8 8 8 4 4 4 4 8 . 
+        . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . 8 8 2 2 2 8 8 8 8 8 2 2 2 2 . 
+        . . 8 2 2 2 2 2 8 8 8 2 2 2 2 2 
+        . . . 2 2 2 2 2 . . . 2 2 2 2 2 
+        `, SpriteKind.Player)
+    mySprite.say(sadPhrases._pickRandom(), 500)
+    controller.moveSprite(mySprite, 100, 0)
+    mySprite.ay = 500
+    scene.cameraFollowSprite(mySprite)
+    return mySprite
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         mySprite.vy = -230
@@ -11,24 +36,26 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
-    game.over(false, effects.slash)
+    InvincibleFlash()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
-    game.over(false, effects.slash)
+    InvincibleFlash()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`tile2`, function (sprite, location) {
-    game.over(false, effects.slash)
+    InvincibleFlash()
 })
 sprites.onDestroyed(SpriteKind.Invincible, function (sprite) {
-    mySprite.setKind(SpriteKind.Player)
-    mySprite.setPosition(sprite.x, sprite.y)
+    CreatePlayer().setPosition(sprite.x, sprite.y)
 })
 function InvincibleFlash () {
+    info.changeLifeBy(-1)
+    FXhurt.play()
     mySprite.setKind(SpriteKind.Invincible)
-    mySprite.destroy(effects.disintegrate, 500)
+    mySprite.startEffect(effects.coolRadial)
+    mySprite.lifespan = 1000
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTiles.tile`, function (sprite, location) {
-    game.over(false, effects.slash)
+    InvincibleFlash()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy(effects.confetti, 100)
@@ -36,44 +63,28 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
         sprite.vy = -230
         FXbounce.play()
     } else {
-        info.changeLifeBy(-1)
-        FXhurt.play()
         InvincibleFlash()
     }
 })
-let myEnemy: Sprite = null
 let mySprite: Sprite = null
+let myEnemy: Sprite = null
 let FXbounce: SoundBuffer = null
-let FXhurt: SoundBuffer = null
 let FXjump: SoundBuffer = null
+let FXhurt: SoundBuffer = null
+let sadPhrases: string[] = []
+sadPhrases = [
+"not fair",
+"oh no!",
+"too sad",
+"what a bummer",
+"not cool",
+"this game stinks"
+]
+FXhurt = soundEffects.createSound(soundEffects.waveNumber(WaveType.Sine), 200, 1200, 200, 255, 100)
 FXjump = soundEffects.createSound(soundEffects.waveNumber(WaveType.Triangle), 250, 80, 1000)
-FXhurt = soundEffects.createSound(soundEffects.waveNumber(WaveType.Sine), 333, 1400, 60, 255, 100)
 FXbounce = soundEffects.createSound(soundEffects.waveNumber(WaveType.Cycle32), 150, 440, 1000)
 scene.setBackgroundColor(15)
 tiles.setTilemap(tilemap`platformer1`)
-mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
-    . . . 8 8 8 8 8 8 8 8 8 8 8 . . 
-    8 8 8 8 8 8 8 c 8 8 8 c 8 8 . . 
-    . 8 8 8 8 8 1 1 c 8 c 1 1 1 8 . 
-    . 8 8 8 8 1 1 f 1 c 8 1 f 1 8 . 
-    8 8 8 8 8 8 1 1 1 8 8 1 1 1 8 . 
-    . 8 8 8 8 8 9 8 8 8 8 8 9 8 8 . 
-    8 8 8 8 8 8 9 8 8 8 9 9 9 8 f f 
-    . 8 8 8 4 8 8 4 4 4 9 4 4 f f f 
-    . 1 1 8 4 8 8 4 4 4 9 4 4 4 8 . 
-    . 1 1 4 4 8 8 8 8 8 4 4 4 4 8 . 
-    . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
-    . 8 8 2 2 2 8 8 8 8 8 2 2 2 2 . 
-    . . 8 2 2 2 2 2 8 8 8 2 2 2 2 2 
-    . . . 2 2 2 2 2 . . . 2 2 2 2 2 
-    `, SpriteKind.Player)
-mySprite.say("oh so sad", 500)
-controller.moveSprite(mySprite, 100, 0)
-mySprite.ay = 500
-scene.cameraFollowSprite(mySprite)
-tiles.placeOnRandomTile(mySprite, assets.tile`tile3`)
 for (let value of tiles.getTilesByType(assets.tile`tile5`)) {
     myEnemy = sprites.create(img`
         ................
@@ -105,6 +116,8 @@ for (let value of tiles.getTilesByType(assets.tile`tile5`)) {
     tiles.placeOnTile(myEnemy, value)
     myEnemy.ay = 500
 }
+CreatePlayer()
+tiles.placeOnRandomTile(mySprite, assets.tile`tile3`)
 game.onUpdate(function () {
     for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
         if (!(value.tileKindAt(TileDirection.Left, assets.tile`transparency16`))) {
